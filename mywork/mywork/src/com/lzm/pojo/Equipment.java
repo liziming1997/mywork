a2b19f61b70eafea588c1253cac696be6168a056
liziming1997 <571880799@qq.com>
2018-06-29 09:53:41 +0800

    20118-6-29提交

---
+++ mywork/mywork/src/com/lzm/pojo/Equipment.java
@@ -0,0 +1,68 @@
+package com.lzm.pojo;
+
+/**
+ * @author LZM
+ *
+ */
+public class Equipment {
+    private Integer eqId;
+    private String eqName;
+    private Integer eqWarehouseId;
+    private Integer eqClassId;
+    private Integer eqAmount;
+    private Integer eqSurplus;
+    
+
+    @Override
+	public String toString() {
+		return "Equipment [eqId=" + eqId + ", eqName=" + eqName
+				+ ", eqWarehouseId=" + eqWarehouseId + ", eqClassId="
+				+ eqClassId + ", eqAmount=" + eqAmount  + ", eqSurplus=" + eqSurplus + "]";
+	}
+	
+	public Integer getEqSurplus() {
+		return eqSurplus;
+	}
+	public void setEqSurplus(Integer eqSurplus) {
+		this.eqSurplus = eqSurplus;
+	}
+	public Integer getEqAmount() {
+		return eqAmount;
+	}
+	public void setEqAmount(Integer eqAmount) {
+		this.eqAmount = eqAmount;
+	}
+	public Integer getEqId() {
+        return eqId;
+    }
+    public void setEqId(Integer eqId) {
+        this.eqId = eqId;
+    }
+
+    public String getEqName() {
+        return eqName;
+    }
+
+    public void setEqName(String eqName) {
+        this.eqName = eqName == null ? null : eqName.trim();
+    }
+
+    public Integer getEqWarehouseId() {
+        return eqWarehouseId;
+    }
+
+    
+	public void setEqWarehouseId(Integer eqWarehouseId) {
+        this.eqWarehouseId = eqWarehouseId;
+    }
+
+    
+
+    public Integer getEqClassId() {
+        return eqClassId;
+    }
+
+    public void setEqClassId(Integer eqClassId) {
+        this.eqClassId = eqClassId;
+    }
+}
